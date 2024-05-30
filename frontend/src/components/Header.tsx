10e04430b50424bf2249485c4a311142b8bbd3df
Amr Mamdouh <amrmamdoh@hotmail.com>
2024-05-30 04:26:45 +0300

    Add Link Container for routing elements

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,23 +3,31 @@ import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
 import { FaShoppingCart, FaUser } from "react-icons/fa"
 import logo from "../assets/logo.png"
+import { LinkContainer } from "react-router-bootstrap"
+
 const Header = () => {
   return (
     <Navbar expand="lg" bg="dark" variant="dark" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="/">
-          <img src={logo} alt="proShop" />
-          ProShop
-        </Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="proShop" />
+            ProShop
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/cart">
-              <FaShoppingCart /> Cart
-            </Nav.Link>
-            <Nav.Link href="/login">
-              <FaUser /> Sign In
-            </Nav.Link>
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>
+                <FaUser /> Sign In
+              </Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
