420279d7f430a09f414140871c7d06df6decd9e4
Amr Mamdouh <amrmamdoh@hotmail.com>
2024-06-08 18:53:07 +0300

    Add items counter in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,8 +4,13 @@ import Navbar from "react-bootstrap/Navbar"
 import { FaShoppingCart, FaUser } from "react-icons/fa"
 import logo from "../assets/logo.png"
 import { LinkContainer } from "react-router-bootstrap"
+import { Badge } from "react-bootstrap"
+import { useSelector } from "react-redux"
 
 const Header = () => {
+  const { cartItems } = useSelector(
+    (state: { cart: { cartItems: any[] } }) => state.cart
+  )
   return (
     <Navbar expand="lg" bg="dark" variant="dark" collapseOnSelect>
       <Container>
@@ -21,6 +26,14 @@ const Header = () => {
             <LinkContainer to="/cart">
               <Nav.Link>
                 <FaShoppingCart /> Cart
+                {cartItems.length > 0 && (
+                  <Badge bg="success" style={{ marginLeft: " 0.5rem" }}>
+                    {cartItems.reduce(
+                      (acc: number, x: { qty: number }) => acc + x.qty,
+                      0
+                    )}
+                  </Badge>
+                )}
               </Nav.Link>
             </LinkContainer>
             <LinkContainer to="/login">
