084e3600a00b4b23e43b77f75d667f5eb47f7d5a
Amr Mamdouh <amrmamdoh@hotmail.com>
2024-07-12 18:00:53 +0300

    Add Admin Edit Users Screen

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -29,6 +29,7 @@ import OrderListScreen from "./screens/OrderListScreen"
 import ProductListScreen from "./screens/ProductListScreen"
 import ProductEditScreen from "./screens/ProductEditScreen"
 import UserListScreen from "./screens/UserListScreen"
+import UserEditScreen from "./screens/UserEditScreen"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -50,6 +51,7 @@ const router = createBrowserRouter(
         <Route path="/admin/productlist" element={<ProductListScreen />} />
         <Route path="/admin/userlist" element={<UserListScreen />} />
         <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
+        <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
       </Route>
     </Route>
   )
